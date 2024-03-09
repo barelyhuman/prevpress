4bca751716800d1bd1a798fe4fae7c6255756b94
barelyhuman <sidgroks@gmail.com>
2024-03-09 13:26:25 +0000

    Deploying to gh-pages from @ barelyhuman/prevpress@29206742873cbcc370adde54f71d0c376b018a9a 🚀

---
+++ public/chunks/styling-OGJC7DIA.js
@@ -0,0 +1,26 @@
+import {
+  u
+} from "./chunk-QI2BAEPM.js";
+
+// dist/.cache/guides/styling.js
+function _createMdxContent(props) {
+  const _components = {
+    h1: "h1",
+    ...props.components
+  };
+  return u(_components.h1, {
+    children: "Styling my day"
+  });
+}
+function MDXContent(props = {}) {
+  const { wrapper: MDXLayout } = props.components || {};
+  return MDXLayout ? u(MDXLayout, {
+    ...props,
+    children: u(_createMdxContent, {
+      ...props
+    })
+  }) : _createMdxContent(props);
+}
+export {
+  MDXContent as default
+};
---
+++ public/client-BBFDE7QK.js
@@ -0,0 +1,10 @@
+import {
+  E,
+  u
+} from "./chunks/chunk-QI2BAEPM.js";
+
+// runtime/client.js
+var Component = await import("./chunks/styling-OGJC7DIA.js").then(
+  (mod) => "default" in mod ? mod.default : mod
+);
+E(/* @__PURE__ */ u(Component, {}), document.getElementById("root"));
